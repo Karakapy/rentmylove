87678713a6412c71a75b08d96090d46ed54c485f
nutdaran <nutdaran@hotmail.com>
2023-11-03 23:39:25 +0700

    add State()

---
+++ lib/screens/add_order_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddOrderScreen extends StatefulWidget {
+  const AddOrderScreen({super.key});
+
+  @override
+  State<AddOrderScreen> createState() => _AddOrderScreenState();
+}
+
+class _AddOrderScreenState extends State<AddOrderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/list_of_product_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ListOfProductScreen extends StatefulWidget {
+  const ListOfProductScreen({super.key});
+
+  @override
+  State<ListOfProductScreen> createState() => _ListOfProductScreenState();
+}
+
+class _ListOfProductScreenState extends State<ListOfProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
