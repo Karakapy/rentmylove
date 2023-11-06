fc17d55d5b95380464686ee6820a84c2a1815802
nutdaran <nutdaran@hotmail.com>
2023-11-07 00:26:41 +0700

    add color preset for project

---
+++ lib/color_preset.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const wallpaper = Color(0xffFAF3ED);
+const brown = Color(0xff9A715B);
+const green = Color(0xff60CC45);
+const lightGreen = Color(0xffC7F4BB);
+const grey = Color(0xff727272);
+const text = Colors.black;
+const red = Color(0xffCC4545);
+const lightBrown = Color(0xffDBC9B9);
