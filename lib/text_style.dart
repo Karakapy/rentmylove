a2c54410a3e3e3974597dbf78a17d3f7d070be1a
nutdaran <nutdaran@hotmail.com>
2023-11-18 23:51:40 +0700

    add app_bar_widget and text style

---
+++ lib/text_style.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'color_preset.dart';
+
+class RmlTextStyle {
+  static const TextStyle title = TextStyle(
+    color: text,
+    fontSize: 41.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle sectionTitle = TextStyle(
+    color: text,
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle normalText = TextStyle(
+    color: text,
+    fontSize: 17,
+    fontWeight: FontWeight.normal,
+  );
+}
---
+++ lib/widgets/app_bar_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      toolbarHeight: 97,
+      systemOverlayStyle: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+      leading: Align(
+        alignment: Alignment.bottomLeft,
+        child: IconButton(
+          padding: EdgeInsets.only(left: 20),
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(97);
+}
